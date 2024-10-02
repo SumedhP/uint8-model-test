6ec7356eb4435536d69545a0eeeed2e77d4da273
SumedhP <43325361+SumedhP@users.noreply.github.com>
2024-10-01 17:01:30 -0700

    It makes video on closure

--- all_images/label images.py
+++ all_images/label images.py
@@ -2,10 +2,17 @@ from model import labelImage
 import os
 import cv2
 from tqdm import tqdm
+import signal
 
 # Open all the files in the directory
 FILE_DIRECTORY = 'image_store_raw'
 OUTPUT_DIRECTORY = 'labelled'
+
+def handler(sig, frame):
+  makeVideo()
+  exit(0)
+
+signal.signal(signal.SIGINT, handler)
 
 files = os.listdir(FILE_DIRECTORY)
 
@@ -13,16 +20,17 @@ print("Found, ", len(files), " files")
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # For MP4 format
 def makeVideo():
+  print("Making video")
   files = os.listdir(OUTPUT_DIRECTORY)
   output_video_name = str(f'{0}_{len(files)}.mp4')
   videoWriter = cv2.VideoWriter(output_video_name, fourcc, 30, (960, 540))
-  for file in tqdm(files):
+  for file in files:
     image = cv2.imread(os.path.join(OUTPUT_DIRECTORY, file))
     videoWriter.write(image)
   videoWriter.release()
 
 count = 0
-for file in tqdm(files):
+for file in tqdm(files, mininterval=0.0000000000000000000001):
   # Check if the file is already labelled
   if os.path.exists(os.path.join(OUTPUT_DIRECTORY, file)):
     continue
@@ -33,3 +41,4 @@ for file in tqdm(files):
   
   if(count % 500 == 0):
     makeVideo()
+    print("Made a video as we have labelled 500 images")
