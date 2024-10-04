061e769643e97347e2bccc9be490c3aad357cfac
SumedhP <43325361+SumedhP@users.noreply.github.com>
2024-10-04 00:42:50 -0700

    undid some of the profiler work lol

--- gpu/label_images.py
+++ gpu/label_images.py
@@ -3,35 +3,42 @@ import os
 import cv2
 from tqdm import tqdm
 
-def main():
-  # Open all the files in the directory
-  FILE_DIRECTORY = 'half_dataset'
-  OUTPUT_DIRECTORY = 'half_dataset_output'
+# Open all the files in the directory
+FILE_DIRECTORY = 'half_dataset'
+OUTPUT_DIRECTORY = 'half_dataset_output'
 
-  # Make output dir if it doesn't exist
-  if not os.path.exists(OUTPUT_DIRECTORY):
-    os.makedirs(OUTPUT_DIRECTORY)
+# Make output dir if it doesn't exist
+if not os.path.exists(OUTPUT_DIRECTORY):
+  os.makedirs(OUTPUT_DIRECTORY)
 
-  files = os.listdir(FILE_DIRECTORY)
+files = os.listdir(FILE_DIRECTORY)
 
-  print("Found, ", len(files), " files")
+print("Found, ", len(files), " files")
 
-  for file in files:
-    labelled_image = labelImage(os.path.join(FILE_DIRECTORY, file))
-    cv2.imwrite(os.path.join(OUTPUT_DIRECTORY, file), labelled_image)
+times = []
 
-  print("Finished labelling images")
-  print("Now compiling images into a video")
+for file in files:
+  labelled_image, time = labelImage(os.path.join(FILE_DIRECTORY, file))
+  cv2.imwrite(os.path.join(OUTPUT_DIRECTORY, file), labelled_image)
+  times.append(time)
 
-  # Now compile the images into a mp4 video 
-  fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # For MP4 format
-  videoWriter = cv2.VideoWriter('output.mp4', fourcc, 30, (960, 540))
-  images = [cv2.imread(os.path.join(OUTPUT_DIRECTORY, file)) for file in files]
-  for image in tqdm(images):
-    videoWriter.write(image)
-  videoWriter.release()
-  
-if __name__ == '__main__':
-  main()
+print("Finished labelling images")
+print("Now compiling images into a video")
+
+import matplotlib.pyplot as plt
+
+plt.hist(times, bins=100)
+plt.savefig("time taken per img.png")
+plt.figure("line plot")
+plt.plot(times)
+plt.savefig("time taken per img line.png")
+
+# Now compile the images into a mp4 video 
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # For MP4 format
+videoWriter = cv2.VideoWriter('output.mp4', fourcc, 30, (960, 540))
+images = [cv2.imread(os.path.join(OUTPUT_DIRECTORY, file)) for file in files]
+for image in tqdm(images):
+  videoWriter.write(image)
+videoWriter.release()
 
 
