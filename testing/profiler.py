f153dd42ce56a9de9099cc9f3b5f5206bac6f6e5
SumedhP <43325361+SumedhP@users.noreply.github.com>
2024-11-24 22:19:10 -0800

    this profiler is weird

--- testing/profiler.py
+++ testing/profiler.py
@@ -4,4 +4,4 @@ from line_profiler import load_stats, show_text
 # Load the .lprof file
 stats = load_stats("bench.py.lprof")
 
-print(show_text(stats.timings, unit=1e-6))
+show_text(stats.timings, unit=1e-6, rich=True, output_unit=1e-5)
